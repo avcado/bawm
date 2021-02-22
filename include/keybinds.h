7ebda41978c89deb389a10f4971eeba57b3a04e2
avcado <devavcado@gmail.com>
2021-02-22 11:37:59 -0600

    Add an example file, for keybinds

---
+++ include/keybinds.h
@@ -0,0 +1,22 @@
+/* Keybindings for BAWM */
+
+#pragma once
+#include <X11/Xlib.h>
+
+#ifndef MODMASK
+#define MODMASK
+
+// Mod4Mask / Super
+unsigned int mod4 = Mod4Mask; // 1<<6
+
+#endif // MODMASK
+
+#ifndef KEYBINDS
+#define KEYBINDS
+
+
+// Add your keybinds here.
+// Check the examples before adding keybinds
+// (above these comments)
+
+#endif // KEYBINDS
