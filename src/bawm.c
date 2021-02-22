170b3976d6b24ac20e0e227c994371f1a78865db
avcado <devavcado@gmail.com>
2021-02-22 06:04:14 +0000

    Add some variables, connect to X

--- src/bawm.c
+++ src/bawm.c
@@ -1,6 +1,28 @@
 // BAWM
 
 #include <X11/Xlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
+  // Create a display object
+  Display* disp;
+
+  // Create window attributes
+  XWindowAttrbutes attrib;
+
+  // Create a Button event
+  XButtonEvent butt; // Trying to make things shorter...
+
+  // Create an event for X events
+  XEvent event;
+
+  // Try to open the display,
+  // If I can't, then we want to exit
+  // We'll use 0 instead of 0x00.
+
+  if(!(disp = XOpenDisplay(0))){
+    fprintf(stderr, "Can't open display!");
+    return 1;
+  }
 }
