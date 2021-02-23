0bb20c78ec928cd29211d83f8df0ad37fe1459c7
avcado <devavcado@gmail.com>
2021-02-23 00:51:02 +0000

    Remove janky keybind system

--- include/keybinds.h
+++ include/keybinds.h
@@ -17,44 +17,10 @@ unsigned int mod = Mod4Mask; // Change this to your
 #ifndef KEYBINDS
 #define KEYBINDS
 
-// An example: running alacritty with Super + T
-void alacrittyrun(Display* disp) {
-    if (XGrabKey(disp, XKeysymToKeycode(disp, XStringToKeysym("T")),
-        mod, DefaultRootWindow(disp), True, GrabModeAsync,
-        GrabModeAsync)){
-            // Now we're gonna run alacritty
-            // This is probably bad practice,
-            // But it works
-            system("alacritty");
-        }
-}
-
-// Another example if you'd like it
-// running dmenu with Mod + D
-void dmenurun(Display* disp){
-    if (XGrabKey(disp, XKeysymToKeycode(disp, XStringToKeysym("D")),
-        mod, DefaultRootWindow(disp), True, GrabModeAsync,
-        GrabModeAsync)){
-            // Run it
-            system("dmenu_run");
-        }
-}
-
-// Add your keybinds here.
-// Check the examples before adding keybinds
-// (above these comments)
-
-#endif // KEYBINDS
-
-#ifndef LOAD_KEYBINDS
-#define LOAD_KEYBINDS
-
 void loadKeyBinds(Display* disp){
     // This is an example of running multiple
     // Key handlers.
     
-    alacrittyrun(disp);
-    dmenurun(disp);
 }
 
-#endif // LOAD_KEYBINDS
+#endif // KEYBINDS
