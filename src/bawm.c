ff12daaf9e15a32a0e29d3da6f765c77e2dd46f2
avcado <devavcado@gmail.com>
2021-02-28 22:17:13 -0600

    Support more events

--- src/bawm.c
+++ src/bawm.c
@@ -61,12 +61,37 @@ int main(void) {
     switch (event.type){
       // Key press (or 2, since for some reason
       // Events are integers.)
-      case 2:
+      case 2: // Key press
         // Check if it's a subwindow
         if (event.xkey.subwindow != None){
           XRaiseWindow(disp, event.xkey.subwindow); // Raise window.
         }
         break;
+      case 4: // Button Press
+        if (event.xbutton.subwindow != None){
+          // Get windows attributes
+          XGetWindowAttributes(disp, event.xbutton.subwindow, &attrib);
+          // Set the butt (don't bully me) variable
+          // to ev.xbutton
+          butt = event.xbutton;
+        }
+        break;
+      case 6:
+        if (butt.subwindow != None){
+          // Get difference of both X & Y of the windows
+          int xDiff = event.xbutton.x_root - butt.x_root;
+          int yDiff = event.xbutton.y_root - butt.y_root;
+
+          // Move/resize the window to where the
+          // difference of both x&y
+
+          // NOTE TO SEF: this function is painful.
+          XMoveResizeWindow(disp, butt.window,
+                            attrib.x + (butt.button==1 ? xDiff : 0),
+                            attrib.y + (butt.button==1 ? yDiff : 0),
+                            MAX(1, attrib.width + (butt.button==3 ? xDiff : 0)),
+                            MAX(1, attrib.height + (butt.button==3 ? yDiff :0)));
+        }
       default:
         fprintf(stderr, "Can't handle event! Code: %d\n", event.type);
         return 127;
